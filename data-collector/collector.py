b3c9a4d99edc2ad3f5a93e314d72c8bf87eb3633
Troy D'Amico <troydamico16@gmail.com>
2026-07-29 22:28:34 -0500

    subscribed to mysportsfeeds api, made successful request

--- data-collector/collector.py
+++ data-collector/collector.py
@@ -1,7 +1,28 @@
+import base64
+import os
+
+from dotenv import load_dotenv
 import requests
 
-url = "https://api.sleeper.app/v1/players/nfl"
-response = requests.get(url, timeout=5)
-response.raise_for_status()
-data = response.json()
-print(data)
+load_dotenv()
+
+
+def send_request(url: str, apiKey: str):
+    credentials = base64.b64encode(f"{apiKey}:MYSPORTSFEEDS".encode()).decode("utf-8")
+    response = requests.get(
+        url,
+        params={"fordate": "20161121"},
+        headers={"Authorization": "Basic " + credentials},
+    )
+    print(
+        "Response HTTP Status Code: {status_code}".format(
+            status_code=response.status_code
+        )
+    )
+    print("Response HTTP Response Body: {content}".format(content=response.content))
+
+
+apiKey = os.getenv("API_KEY")
+
+url = "https://api.mysportsfeeds.com/v2.1/pull/nfl/2026-2027-regular/week/1/dfs_projections.json"
+send_request(url, apiKey)
