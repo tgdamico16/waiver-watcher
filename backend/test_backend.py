ffd2a5613a9ac340e73b6ecefe9f423ef018efff
Troy D'Amico <troydamico16@gmail.com>
2026-08-13 23:19:19 -0500

    fixed test

--- backend/test_backend.py
+++ backend/test_backend.py
@@ -17,15 +17,18 @@ def test_update_statistics():
     mock_channel = MagicMock()
     mock_connection = MagicMock()
     mock_connection.channel.return_value = mock_channel
-    app.state.rabbitmq_connection = mock_connection
+    with patch(
+        "main.get_rabbitmq_connection_with_retries"
+    ) as mock_get_rabbit_connection:
+        mock_get_rabbit_connection.return_value = mock_connection
 
-    response = client.get("/update-statistics")
+        response = client.get("/update-statistics")
 
-    _, kwargs = mock_channel.basic_publish.call_args
-    job_id = kwargs["body"]
+        _, kwargs = mock_channel.basic_publish.call_args
+        job_id = kwargs["body"]
 
-    assert response.status_code == 200
-    assert response.json() == {"status": "success", "job_id": job_id}
+        assert response.status_code == 200
+        assert response.json() == {"status": "success", "job_id": job_id}
 
 
 def test_get_job_status():
@@ -36,7 +39,11 @@ def test_get_job_status():
     app.state.postgres_connection = mock_connection
 
     test_job_id = str(uuid.uuid4())
-    response = client.get(f"/job-status/{test_job_id}")
 
-    assert response.status_code == 200
-    assert response.json() == {"status": "success", "job_status": "complete"}
+    with patch("main.get_db_connection_with_retries") as mock_get_db_connection:
+        mock_get_db_connection.return_value = mock_connection
+
+        response = client.get(f"/job-status/{test_job_id}")
+
+        assert response.status_code == 200
+        assert response.json() == {"status": "success", "job_status": "complete"}
