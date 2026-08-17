d7881c1ea2350ce8c4b47723c8222f9fbdd2f2ae
Troy D'Amico <troydamico16@gmail.com>
2026-08-16 19:40:29 -0500

    added health checker

---
+++ health-checker/health_checker.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+import os
+import time
+from dotenv import load_dotenv
+import requests
+import smtplib
+from email.mime.text import MIMEText
+
+load_dotenv()
+
+FRONTEND_HEALTH_URL = "http://waiver_watcher_frontend:3000/api/health"
+ALERT_EMAIL = os.getenv("EMAIL")
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTP_USER = os.getenv("EMAIL")
+SMTP_PASS = os.getenv("GOOGLE_APP_PASSWORD")
+
+
+def send_alert():
+    msg = MIMEText("Waiver Watcher Health Check Failed")
+    msg["Subject"] = "Waiver Watcher Health Check Failed"
+    msg["From"] = SMTP_USER
+    msg["To"] = ALERT_EMAIL
+
+    with smtplib.SMTP(SMTP_HOST, SMTP_PORT) as server:
+        server.starttls()
+        server.login(SMTP_USER, SMTP_PASS)
+        server.sendmail(SMTP_USER, ALERT_EMAIL, msg.as_string())
+
+
+def check_health():
+    try:
+        response = requests.get(FRONTEND_HEALTH_URL)
+        if response.status_code != 200:
+            raise Exception("Health check failed")
+        else:
+            print(f"{datetime.now()} Healthy")
+    except Exception as e:
+        send_alert()
+        raise e
+
+
+def main():
+    while True:
+        check_health()
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    main()
