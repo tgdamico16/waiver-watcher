490bd6b3206bb6a4658602f60316a9c645df6ad9
Troy D'Amico <troydamico16@gmail.com>
2026-08-17 22:39:34 -0500

    added more logging to frontend health check

--- frontend/app/api/health/route.ts
+++ frontend/app/api/health/route.ts
@@ -4,16 +4,21 @@ import { NextResponse } from "next/server";
 const API_HOST = "waiver_watcher_backend:8000";
 
 export async function GET() {
+  console.log("Checking application health");
   try {
     const backend_healthy = await fetch(`http://${API_HOST}/health`);
     const backend_healthy_result = (await backend_healthy.json()) as {
       status: "healthy" | "unhealthy";
     };
     if (backend_healthy_result.status === "healthy") {
+      console.log("Application healthy");
       return NextResponse.json({ status: "healthy" }, { status: 200 });
     }
+    console.log("Application unhealthy, backend response not healthy");
     return NextResponse.json({ status: "unhealthy" }, { status: 500 });
-  } catch {
+  } catch (e) {
+    console.log("Application unhealthy, error encountered");
+    console.error(e);
     return NextResponse.json({ status: "unhealthy" }, { status: 500 });
   }
 }
